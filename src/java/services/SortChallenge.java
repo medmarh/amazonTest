e7c9063dcfc5d90e77c23e61a269fe71c5feb78e
unknown <medmarh@gmail.com>
2020-04-12 12:50:36 -0500

    deleted uncessary files

--- src/java/services/SortChallenge.java
+++
@@ -1,8 +0,0 @@
-package java.services;
-
-public interface SortChallenge {
-    public int[] simpleSort(int[] list, boolean ascending);
-    public void printSortedFrequency(int[] list);
-    public int numberOfUniqueValues(int[] list);
-}
-
